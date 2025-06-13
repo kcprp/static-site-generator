aea0d8335baaf982dda08d66f99c36e70d8438e4
kcprp <kp.szkola@gmail.com>
2025-06-13 22:11:23 +0100

    copy files from static to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_files(source, destination):
+    if not os.path.exists(source):
+        raise Exception("Source path does not exist")
+    
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.makedirs(destination)
+    
+    for item in os.listdir(source):
+        src_path = os.path.join(source, item)
+        dst_path = os.path.join(destination, item)
+        
+        if os.path.isfile(src_path):
+            shutil.copy2(src_path, dst_path)
+        else:
+            shutil.copytree(src_path, dst_path)
+    
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.google.com")
-    print(text_node)
+    source = "static"
+    destination = "public"
+    
+    copy_files(source, destination)
 
 if __name__ == "__main__":
     main()
